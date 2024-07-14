00258f3047964f8162099e8c824162e37e6a5d72
Romeo <popescuromeo22@stud.ase.ro>
2024-07-14 18:48:52 +0300

    Removed the part and content components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,28 +8,6 @@ const Total = (props) => {
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
-// const Part = (props) => {
-//   console.log(props)
-//   return (
-//     <div>
-//       <p>{props.part} {props.exercise}</p>
-//     </div>
-//   )
-// }
-// const Content = (props) => {
-//   console.log(props)
-//   return (
-//     <>
-//     <div>
-//       <Part part={props.part[0].part} exercise={props.part[0].exercise}/>
-//       <Part part={props.part[1].part} exercise={props.part[1].exercise}/>
-//       <Part part={props.part[2].part} exercise={props.part[2].exercise}/>
-//     </div>  
-       
-//     </>
-    
-//   )
-// }
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -38,16 +16,11 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-  const parts = [
-    {part: {part1}, exercise: {exercises1}},
-    {part: {part2}, exercise: {exercises2}},
-    {part: {part3}, exercise: {exercises3}},
-  ]
+  
 
    return (
     <div>
       <Header course={course}/> 
-      {/* <Content part={parts}/> */}
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       
     </div>
