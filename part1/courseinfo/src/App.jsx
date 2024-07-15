0b01fba828b0deb4d582065fbeb08609560e4aa2
Romeo <popescuromeo22@stud.ase.ro>
2024-07-15 12:28:30 +0300

    Finished exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,6 +8,23 @@ const Total = (props) => {
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
+const Content = ({parts}) => {
+  return (
+    <p>
+      <Part part={parts[0].part} exercise={parts[0].exercise}/>
+      <Part part={parts[1].part} exercise={parts[1].exercise}/>
+      <Part part={parts[2].part} exercise={parts[2].exercise}/>
+    </p>
+
+  )
+}
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -16,11 +33,16 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-  
+  const parts = [
+    {part: part1, exercise: exercises1},
+    {part: part2, exercise: exercises2},
+    {part: part3, exercise: exercises3}
+  ] 
 
    return (
     <div>
       <Header course={course}/> 
+      <Content parts={parts} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       
     </div>
