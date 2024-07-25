ed2a8cf392062c5e01f7ce49c4cc66bac33eae30
Romeo <popescuromeo22@stud.ase.ro>
2024-07-25 18:04:29 +0300

    Commit

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -39,36 +39,42 @@ const App = () => {
       name: newName,
       number: newNumber,
     }
-    setNewName(' ')
-    setNewNumber(' ')
-    setPersons(persons.concat(newObject))
-  }
-  //handle the name change in input (takes what you write)
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-  const handleSearchChange = (event) => {
-    setNewSearch(event.target.value)
-  }
-  //function that shows the names
-  const namesToShow = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
-  return (
-    
-    <div>
-      <h1>Phonebook</h1>
-        <Filter value={search} onChange={handleSearchChange}/>
-      <h1>add a new</h1>
-       <PersonForm addInfo={addInfo} newName={newName} newNumber={newNumber}
-          handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/> 
-      <h1>Numbers</h1>
-      <div>
-        <Persons namesToShow={namesToShow} />
-      </div>
-    </div>
-  )
+
+    axios
+      .post('http://localhost:3001/persons', newObject)
+      .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName(' ')
+          setNewNumber(' ')
+})
+  
+}
+//handle the name change in input (takes what you write)
+const handleNameChange = (event) => {
+setNewName(event.target.value)
+}
+const handleNumberChange = (event) => {
+setNewNumber(event.target.value)
+}
+const handleSearchChange = (event) => {
+setNewSearch(event.target.value)
+}
+//function that shows the names
+const namesToShow = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+return (
+
+<div>
+<h1>Phonebook</h1>
+  <Filter value={search} onChange={handleSearchChange}/>
+<h1>add a new</h1>
+<PersonForm addInfo={addInfo} newName={newName} newNumber={newNumber}
+    handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/> 
+<h1>Numbers</h1>
+<div>
+  <Persons namesToShow={namesToShow} />
+</div>
+</div>
+)
 }
 
-export default App
+export default App                                                                                                                                                                                                                                                                                   
